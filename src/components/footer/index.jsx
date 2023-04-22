61bd54adba1d5aae44e0c9c58f94c71a5f20d1aa
sa96125 <sa96125@gmail.com>
2023-04-22 17:04:14 +0900

    [fix] 깃허브 링크 주소 변경

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,7 +4,8 @@ import './index.scss'
 
 export const Footer = () => (
   <footer className="footer">
-    ©<a href="https://github.com/JaeYeopHan">Jbee</a>, Built with{' '}
+    ©<a href="https://github.com/sa96125/jongseung-link">Jongseung.link</a>,
+    Built with{' '}
     <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
       Gatsby-starter-bee
     </a>
